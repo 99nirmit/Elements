cd11a2b312d3a163b1d268c412a3faadec49dbbd
Nirmit <user.email>
2023-04-13 12:34:55 +0530

    ng-content,container,template topics

---
+++ src/app/app.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'elements';
+  
+  numbers=[1,2,3,4,5,6,7,8,9,10]
+}
